9fb7a8a1d16c79c3d65bc8a1b71fa43211e4133a
notonich <lyamin.dv1981@gmail.com>
2026-05-06 23:12:50 +0300

    Добавлена регистрация + основа главной страницы.

---
+++ WEB Project/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/registration',methods=['GET','POST'])
+def registration():
+    if request.method == 'GET':
+        return render_template('registration.html')
+    elif request.method == 'POST':
+        name = request.form['name']
+        surname = (request.form['last-name'])
+        return redirect(url_for('main_page'))
+
+
+@app.route('/main')
+@app.route('/main/<account_name>')
+def main_page(account_name=None):
+    print(account_name)
+    return render_template('main_page.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
