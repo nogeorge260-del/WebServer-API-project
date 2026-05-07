7638d79a514a5f376cdad51f9906a12e8f71670a
notonich <lyamin.dv1981@gmail.com>
2026-05-07 21:33:15 +0300

    Начало работы с бд.

--- WEB Project/main.py
+++ WEB Project/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -15,10 +16,15 @@ def registration():
 
 
 @app.route('/main')
-@app.route('/main/<account_name>')
+# @app.route('/main/<account_name>')
 def main_page(account_name=None):
     print(account_name)
     return render_template('main_page.html')
+
+
+@app.route('/main/register_listing')
+def register_product():
+    return render_template('register_listing.html')
 
 
 if __name__ == '__main__':
