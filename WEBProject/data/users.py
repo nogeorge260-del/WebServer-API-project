836e623930b1ab46ed6d90946545cc0ba0fa2900
Артём <mitrofanov.a@sch2009.net>
2026-05-14 20:54:42 +0300

    Сделал рабочую регистрацию, вход и добавление продуктов

---
+++ WEBProject/data/users.py
@@ -0,0 +1,30 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    lastname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=False)
+    phone = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    gender = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
---
+++ WEBProject/main.py
@@ -0,0 +1,137 @@
+import sqlite3
+import os
+from flask import Flask, request, render_template, redirect, url_for
+from data import users
+from data import db_session
+from werkzeug.security import generate_password_hash
+from werkzeug.utils import secure_filename
+from forms.user import RegisterForm
+from forms.login import LoginForm
+from forms.listing import ListingForm
+from data.users import User
+from data.listings import Listings
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.get(User,user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/main")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            lastname=form.lastname.data,
+            email=form.email.data,
+            about=form.about.data,
+            phone=form.phone.data,
+            age=form.age.data,
+            gender=form.gender.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        print('1')
+        return redirect('/main')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/main',methods=['GET','POST'])
+#@app.route('/main/<page')
+# @app.route('/main/<account_name>')
+def main_page(page=1):
+    if request.method == 'GET':
+        con = sqlite3.connect("db/user_listing_info.db")
+        cur = con.cursor()
+        result = cur.execute(f"""SELECT name, about FROM listings""").fetchall()
+        counter = 0
+        show = []
+        for elem in result:
+            if counter < 5:
+                show.append(elem)
+            else:
+                break
+            counter += 1
+        while counter < 5:
+            show.append(['Листингов больше нет', '...'])
+            counter += 1
+        con.close()
+        return render_template('main_page.html', listing1=show[0],
+                               listing2=show[1], listing3=show[2], listing4=show[3], listing5=show[4])
+    elif request.method == 'POST':
+        pass
+
+
+@app.route('/main/register_listing', methods=['GET', 'POST'])
+def register_product():
+    form = ListingForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+
+        file = form.photo.data
+
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            upload_dir = os.path.join('static', 'img', 'products')
+            os.makedirs(upload_dir, exist_ok=True)
+            full_path = os.path.join(upload_dir, filename)
+            file.save(full_path)
+            file_path = f"/static/img/products/{filename}"
+
+        listing = Listings(
+            name=form.name.data,
+            about=form.about.data,
+            email=form.email.data,
+            phone_number=form.phone_number.data,
+            photo=file_path
+        )
+
+        db_sess.add(listing)
+        db_sess.commit()
+
+        return redirect('/main')
+    return render_template('register_listing.html', form=form)
+
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/user_listing_info.db")
+    app.run(port=8080, host='127.0.0.1', debug=True)
